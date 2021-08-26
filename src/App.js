315a5584e326e69715384ac57f66a3053c328ac2
Eva Demkova <eva.demkova@firma.seznam.cz>
2021-08-26 17:35:05 +0200

    wtf?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import "./components/style.css";
+
+import Navigation from "./components/navbar";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import { AuthProvider } from "./Auth";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <main>
+        <Navigation />
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Router>
+      </main>
+    </AuthProvider>
   );
 }
 
