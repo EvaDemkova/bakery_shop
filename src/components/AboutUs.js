50632262365c2f5248f400835c4a4bfad6db04d7
Eva Demkova <eva.demkova@firma.seznam.cz>
2021-09-14 14:34:52 +0200

    ?

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -7,30 +7,23 @@ const AboutUs = () => {
       <div className="about-us item-2">
         <h3>About us</h3>
         <p>
-          Our bakery has a special focus on sustainable fair trade and organic
-          products, our selection is made for good health, and we're totally
-          gluten-free. We also make a fabulous selection of teas and coffees,
-          and we're happy to discuss new ideas with you. We're looking forward
-          to meeting you and learning more about what makes us the best!
+          Our bakery has a sxsdasdas ' asdasd'pecial focus on sustainable fair
+          trade and organic products, our selection is made for good health, and
+          we're totally gluten-free. We also make a fabulous selection of teas
+          and coffees, and we're happy to discuss new ideas with you. We're
+          looking forward to meeting you and learning more about what makes us
+          the best!
         </p>
       </div>
 
       <div className="about-us item-2">
         <h3>About us</h3>
-        <p>
-          Our bakery has a special focus on sustainable fair trade and organic
-          products, our selection is made for good health, and we're totally
-          gluten-free. We also make a fabulous selection of teas and coffees,
-          and we're happy to discuss new ideas with you. We're looking forward
-          to meeting you and learning more about what makes us the best!
-        </p>
       </div>
 
       <div className="about-us item-2">
         <h3>About us</h3>
         <p>
-          Our bakery has a special focus on sustainable fair trade and organic
-          products, our selection is made for good health, and we're totally
+          {' '}
           gluten-free. We also make a fabulous selection of teas and coffees,
           and we're happy to discuss new ideas with you. We're looking forward
           to meeting you and learning more about what makes us the best!
